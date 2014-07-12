2f5d3872c0e5a67966d5c3b96083a2db406732cc
Lefebvre Arnaud <a.lefebvre@outlook.fr>
2014-07-13 00:20:53 +0200

    Fix indentation (2 spaces to 4 spaces)

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 var express = require('express'),
-	app = express(),
-	Parser = require('./lib/Parser.js'),
-	path = require('path'),
-	open = require('open');
+    app = express(),
+    Parser = require('./lib/Parser.js'),
+    path = require('path'),
+    open = require('open');
 
 Parser.init();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/list', function(req, res){
-	Parser.readDir(req.query.dir, req.query.type).map(function(data){
-		res.send(200, data);
-	}).mapError(function(err){
-		if(err.error === 'unreadable'){
-			res.send(403, err.error);
-		} else if(err.error === 'wrong_folder_name'){
-			res.send(400, err.error);
-		} else{
-			res.send(500);
-		}
-	});
+    Parser.readDir(req.query.dir, req.query.type).map(function(data){
+        res.send(200, data);
+    }).mapError(function(err){
+        if(err.error === 'unreadable'){
+            res.send(403, err.error);
+        } else if(err.error === 'wrong_folder_name'){
+            res.send(400, err.error);
+        } else{
+            res.send(500);
+        }
+    });
 });
 
 app.listen(3000, function(){
-//	console.log('Your browser should be opened at "http://127.0.0.1:3000". If not, copy the given URL and paste it on your brower\'s bar');
-//	open('http://127.0.0.1:3000');
+    console.log('Your browser should be opened at "http://127.0.0.1:3000". If not, copy the given URL and paste it on your brower\'s bar');
+    open('http://127.0.0.1:3000');
 });
--- public/index.js
+++ public/index.js
@@ -1,40 +1,40 @@
 function Client(){
-	
-	this.templates = {
-		files: _.template('<li><%= name %></li>'),
-		folders: _.template('<li><a href="#" class="folder"><%= name %></a></li>'),
-		errors: _.template('<li><strong><%= error %></strong></li>'),
-	  devices: _.template('<li><a href="#" class="device"><%= name %></a></li>')
+    
+    this.templates = {
+        files: _.template('<li><%= name %></li>'),
+        folders: _.template('<li><a href="#" class="folder"><%= name %></a></li>'),
+        errors: _.template('<li><strong><%= error %></strong></li>'),
+      devices: _.template('<li><a href="#" class="device"><%= name %></a></li>')
   };
-	
-	var self = this;
-	
-	
-	this.display = function(data){
-		$('#files-list, #devices-list').empty();
-		
-		_.each(data, function(file){
-			if(file.type === 'file'){
-				$(self.templates.files({
-					name: file.name
-				})).appendTo($('#files-list'));
-			} else if(file.type === 'folder'){
-				$(self.templates.folders({
-					name: file.name
-				})).data('folderName', file.name).appendTo($('#files-list'));
-			} else if(file.type === 'device'){
+    
+    var self = this;
+    
+    
+    this.display = function(data){
+        $('#files-list, #devices-list').empty();
+        
+        _.each(data, function(file){
+            if(file.type === 'file'){
+                $(self.templates.files({
+                    name: file.name
+                })).appendTo($('#files-list'));
+            } else if(file.type === 'folder'){
+                $(self.templates.folders({
+                    name: file.name
+                })).data('folderName', file.name).appendTo($('#files-list'));
+            } else if(file.type === 'device'){
         $(self.templates.devices({
           name: file.name
         })).data('device', file).appendTo($('#devices-list'));
       } else if(file.type === 'error'){
-				$(self.templates.errors({
-					error: file.error
-				})).appendTo($('#files-list'));
-			}
-		});
-		$('#files-list .folder').on('click', function(){
-			self.getFiles($(this).parents('li').data('folderName'), 'folder');
-		});
+                $(self.templates.errors({
+                    error: file.error
+                })).appendTo($('#files-list'));
+            }
+        });
+        $('#files-list .folder').on('click', function(){
+            self.getFiles($(this).parents('li').data('folderName'), 'folder');
+        });
     $('#devices-list .device').on('click', function(){
       self.getFiles($(this).parents('li').data('device').name, 'device');
     });
@@ -45,16 +45,16 @@ function Client(){
       $('#devices').show();
     }
 
-	};
-	
-	this.getFiles = function(dir, type){
-		dir = dir || null;
+    };
+    
+    this.getFiles = function(dir, type){
+        dir = dir || null;
     type = type || null;
-		$.get('/list', {dir: dir, type: type}).done(_.bind(this.display, this)).fail(_.bind(console.log, console));
-	};
-	
-	this.init = function(){
-		this.getFiles();
+        $.get('/list', {dir: dir, type: type}).done(_.bind(this.display, this)).fail(_.bind(console.log, console));
+    };
+    
+    this.init = function(){
+        this.getFiles();
     return this;
-	};
+    };
 }
