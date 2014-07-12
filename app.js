964bc73905fa6063be8a2f0fa8d297abd3c442d7
Lefebvre Arnaud <a.lefebvre@outlook.fr>
2014-07-12 20:05:00 +0200

    Add linux device parsing

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express'),
 	app = express(),
-	Parser = require('./Parser.js'),
+	Parser = require('./lib/Parser.js'),
 	path = require('path'),
 	open = require('open');
 
+Parser.init();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/list', function(req, res){
-	Parser.readDir(req.query.dir).map(function(data){
+	Parser.readDir(req.query.dir, req.query.type).map(function(data){
 		res.send(200, data);
 	}).mapError(function(err){
 		if(err.error === 'unreadable'){
@@ -21,6 +22,6 @@ app.get('/list', function(req, res){
 });
 
 app.listen(3000, function(){
-	console.log('Your browser should be opened at "http://127.0.0.1:3000". If not, copy the given URL and paste it on your brower\'s bar');
-	open('http://127.0.0.1:3000');
+//	console.log('Your browser should be opened at "http://127.0.0.1:3000". If not, copy the given URL and paste it on your brower\'s bar');
+//	open('http://127.0.0.1:3000');
 });
--- public/index.js
+++ public/index.js
@@ -2,43 +2,59 @@ function Client(){
 	
 	this.templates = {
 		files: _.template('<li><%= name %></li>'),
-		folders: _.template('<li><a href="#" class="folder-list"><%= name %></a></li>'),
-		errors: _.template('<li><strong><%= error %></strong></li>')
-	};
+		folders: _.template('<li><a href="#" class="folder"><%= name %></a></li>'),
+		errors: _.template('<li><strong><%= error %></strong></li>'),
+	  devices: _.template('<li><a href="#" class="device"><%= name %></a></li>')
+  };
 	
 	var self = this;
 	
 	
 	this.display = function(data){
-		$('#files').empty();
+		$('#files-list, #devices-list').empty();
 		
 		_.each(data, function(file){
 			if(file.type === 'file'){
 				$(self.templates.files({
 					name: file.name
-				})).appendTo($('#files'));
+				})).appendTo($('#files-list'));
 			} else if(file.type === 'folder'){
 				$(self.templates.folders({
 					name: file.name
-				})).data('folderName', file.name).appendTo($('#files'));
-			} else if(file.type === 'error'){
+				})).data('folderName', file.name).appendTo($('#files-list'));
+			} else if(file.type === 'device'){
+        $(self.templates.devices({
+          name: file.name
+        })).data('device', file).appendTo($('#devices-list'));
+      } else if(file.type === 'error'){
 				$(self.templates.errors({
 					error: file.error
-				})).appendTo($('#files'));
+				})).appendTo($('#files-list'));
 			}
 		});
-		$('#files .folder-list').on('click', function(){
-			self.getFiles($(this).parents('li').data('folderName'));
+		$('#files-list .folder').on('click', function(){
+			self.getFiles($(this).parents('li').data('folderName'), 'folder');
 		});
+    $('#devices-list .device').on('click', function(){
+      self.getFiles($(this).parents('li').data('device').name, 'device');
+    });
+
+    if($('.device').length <= 0){
+      $('#devices').hide();
+    } else{
+      $('#devices').show();
+    }
+
 	};
 	
-	this.getFiles = function(dir){
+	this.getFiles = function(dir, type){
 		dir = dir || null;
-		$.get('/list', {dir: dir}).done(_.bind(this.display, this)).fail(_.bind(console.log, console));
+    type = type || null;
+		$.get('/list', {dir: dir, type: type}).done(_.bind(this.display, this)).fail(_.bind(console.log, console));
 	};
 	
 	this.init = function(){
 		this.getFiles();
-		return this;
+    return this;
 	};
 }
